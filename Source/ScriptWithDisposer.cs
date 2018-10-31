6c6fd86536ef854be0231af4f537ca0a5679a724
Stefnotch <brandmairstefan@gmail.com>
2018-10-31 12:10:23 +0100

    Little unrelated experiment

---
+++ Source/ScriptWithDisposer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlaxEngine;
+
+namespace CartoonShader.Source
+{
+	public abstract class ScriptWithDisposer : Script
+	{
+		protected readonly Disposer _disposer = new Disposer();
+
+		protected virtual void OnDisable()
+		{
+		}
+
+		protected virtual void OnDestroy()
+		{
+		}
+	}
+}
